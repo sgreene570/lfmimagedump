260a7426c39eecebb6cf2ff05d95dc5be7dc1e91
Stephen Greene <sgreene570@gmail.com>
2017-03-09 01:23:40 -0500

    added script

---
+++ lfmimage.py
@@ -0,0 +1,26 @@
+"""
+Last fm image dump script.
+@author: Stephen Greene
+"""
+
+
+import requests
+import json
+import argparse
+
+
+LFMAPI_URL = "http://ws.audioscrobbler.com/2.0/"
+LFMAPI_KEY = ""
+
+
+def main():
+    parser = argparse.ArgumentParser(description="last fm image dump")
+    parser.add_argument("username", type=str, nargs=1, help="Last.fm username")
+    args = parser.parse_args()
+    top_albums = requests.get(LFMAPI_URL + "?method=user.gettopalbums&user="
+                    + str(args.username) + "&api_key=" + LFMAPI_KEY + "&format=json")
+    print(top_albums.json())
+
+
+if __name__ == "__main__":
+    main()
