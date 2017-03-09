4d88d02cfef10494ee7d429a0f8925b277e5aec5
Stephen Greene <sgreene570@gmail.com>
2017-03-09 16:41:06 -0500

    fixed file ext

--- lfmimage.py
+++ lfmimage.py
@@ -18,11 +18,12 @@ def main():
     parser.add_argument("username", type=str, nargs=1, help="Last.fm username")
     args = parser.parse_args()
     top_albums = requests.get(LFMAPI_URL + "?method=user.gettopalbums&user="
-                    + str(args.username).strip("[']") + "&api_key=" 
+                    + str(args.username).strip("[']") + "&api_key="
                     + LFMAPI_KEY + "&format=json").json()
 
     for album in top_albums["topalbums"]["album"]:
-        urllib.request.urlretrieve(album["image"][1]["#text"], album["name"])
+        urllib.request.urlretrieve(album["image"][1]["#text"],
+                                  (album["name"] + ".png"))
 
 
 
