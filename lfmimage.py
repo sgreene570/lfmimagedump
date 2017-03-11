6f46e16dbc638629f80852ef618b6a4e99f14a71
Stephen Greene <sgreene570@gmail.com>
2017-03-10 21:30:31 -0500

    added album art directory

--- lfmimage.py
+++ lfmimage.py
@@ -8,23 +8,33 @@ import requests
 import json
 import argparse
 import urllib.request
+import os
 
 
 LFMAPI_URL = "http://ws.audioscrobbler.com/2.0/"
+ALBUM_ART_DIRECTORY = "albumart"
+
+
+def get_top_album_art(username):
+    top_albums = requests.get(LFMAPI_URL + "?method=user.gettopalbums&user="
+                            + username + "&api_key=" + LFMAPI_KEY
+                            + "&format=json").json()
+
+    for album in top_albums["topalbums"]["album"]:
+        url = album["image"][2]["#text"].rstrip()
+        if url is not '':
+            if not os.path.exists("albumart"):
+                os.makedirs(ALBUM_ART_DIRECTORY)
+
+            filename = os.path.join(ALBUM_ART_DIRECTORY, album["name"] + ".png")
+            urllib.request.urlretrieve(url, filename)
 
 
 def main():
     parser = argparse.ArgumentParser(description="last fm image dump")
     parser.add_argument("username", type=str, nargs=1, help="Last.fm username")
     args = parser.parse_args()
-    top_albums = requests.get(LFMAPI_URL + "?method=user.gettopalbums&user="
-                    + str(args.username).strip("[']") + "&api_key="
-                    + LFMAPI_KEY + "&format=json").json()
-
-    for album in top_albums["topalbums"]["album"]:
-        url = album["image"][2]["#text"].rstrip()
-        if url is not '':
-            urllib.request.urlretrieve(url, (album["name"] + ".png"))
+    get_top_album_art(str(args.username).strip("[']"))
 
 
 if __name__ == "__main__":
