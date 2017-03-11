2dbedc856d8d88afad648b079ac6534fbb385380
Stephen Greene <sgreene570@gmail.com>
2017-03-10 21:41:58 -0500

    added album art size options

--- lfmimage.py
+++ lfmimage.py
@@ -13,15 +13,20 @@ import os
 
 LFMAPI_URL = "http://ws.audioscrobbler.com/2.0/"
 ALBUM_ART_DIRECTORY = "albumart"
+# Order in which s/m/l images appear in the json object from the lfm api
+SMALL_ART_INDEX = 1
+MEDIUM_ART_INDEX = 2
+LARGE_ART_INDEX = 3
+DEFAULT_SIZE_INDEX = MEDIUM_ART_INDEX
 
 
-def get_top_album_art(username):
+def get_top_album_art(username, size_index):
     top_albums = requests.get(LFMAPI_URL + "?method=user.gettopalbums&user="
                             + username + "&api_key=" + LFMAPI_KEY
                             + "&format=json").json()
 
     for album in top_albums["topalbums"]["album"]:
-        url = album["image"][2]["#text"].rstrip()
+        url = album["image"][size_index]["#text"].rstrip()
         if url is not '':
             if not os.path.exists("albumart"):
                 os.makedirs(ALBUM_ART_DIRECTORY)
@@ -33,8 +38,16 @@ def get_top_album_art(username):
 def main():
     parser = argparse.ArgumentParser(description="last fm image dump")
     parser.add_argument("username", type=str, nargs=1, help="Last.fm username")
+    parser.add_argument("--small", action="store_true")
+    parser.add_argument("--large", action="store_true")
     args = parser.parse_args()
-    get_top_album_art(str(args.username).strip("[']"))
+    size_index = DEFAULT_SIZE_INDEX
+    if args.small:
+        size_index = SMALL_ART_INDEX
+    elif args.large:
+        size_index = LARGE_ART_INDEX
+
+    get_top_album_art(str(args.username).strip("[']"), size_index)
 
 
 if __name__ == "__main__":
