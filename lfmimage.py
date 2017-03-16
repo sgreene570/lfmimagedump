4c92bef980abd3b7cc09acf4aa121686ec43d5cc
Stephen Greene <sgreene570@gmail.com>
2017-03-15 20:26:08 -0400

    error checking

--- lfmimage.py
+++ lfmimage.py
@@ -1,7 +1,7 @@
 """
 Last.fm image dump script.
 Uses the Last.fm api to fetch album arts for user's top 50 albums.
-@author: Stephen Greene
+@author: sgreene570
 """
 
 
@@ -27,6 +27,10 @@ def get_top_album_art(username, size_index):
     top_albums = requests.get(LFMAPI_URL + "?method=user.gettopalbums&user="
                             + username + "&api_key=" + LFMAPI_KEY
                             + "&format=json").json()
+    if "error" in top_albums.keys():
+        print("Error: Check Username?")
+        os._exit(1)
+
     for album in tqdm(top_albums["topalbums"]["album"]):
         url = album["image"][size_index]["#text"].rstrip()
         if url is not '':
@@ -40,8 +44,8 @@ def get_top_album_art(username, size_index):
 def main():
     parser = argparse.ArgumentParser(description="Last.fm image dump")
     parser.add_argument("username", type=str, nargs=1, help="Last.fm username")
-    parser.add_argument("--small", action="store_true")
-    parser.add_argument("--large", action="store_true")
+    parser.add_argument("--small", action="store_true", help="Fetch smaller images")
+    parser.add_argument("--large", action="store_true", help="Fetch larger images")
     args = parser.parse_args()
     size_index = DEFAULT_SIZE_INDEX
     if args.small:
